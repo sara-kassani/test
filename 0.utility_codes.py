0d9ae84940b4aa752d4500d32f885aa36c0bd46c
sara-kassani <kassani.sara@gmail.com>
2019-03-03 18:02:28 -0600

    Update 0.utility_codes.py

--- 0.utility_codes.py
+++ 0.utility_codes.py
@@ -16,7 +16,84 @@
 
 
 ###########################################################################################################################
+# Binary roc curve
+from keras.utils import layer_utils, np_utils
 
+# generate roc curve
+n_classes = 2
+
+# Plot linewidth.
+lw = 2
+
+#convert original class labels to the one-hot-encoding
+y_test = np_utils.to_categorical(test_generator.classes, 2)
+
+Y_test_pred = model.predict_generator(test_generator, test_generator.samples // test_generator.batch_size)
+Y_test_predicted = np.argmax(Y_test_pred, axis=1)
+
+# Compute ROC curve and ROC area for each class
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], Y_test_pred[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# Compute micro-average ROC curve and ROC area
+fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), Y_test_pred.ravel())
+roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+# Compute macro-average ROC curve and ROC area
+
+# First aggregate all false positive rates
+all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+# Then interpolate all ROC curves at this points
+mean_tpr = np.zeros_like(all_fpr)
+for i in range(n_classes):
+    mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+# Finally average it and compute AUC
+mean_tpr /= n_classes
+
+fpr["macro"] = all_fpr
+tpr["macro"] = mean_tpr
+roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+plt.rcParams["axes.grid"] = False
+plt.rcParams.update({'font.size': 12})
+
+plt.figure(figsize=(8,6))
+
+plt.plot(fpr["micro"], tpr["micro"],
+         label='micro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["micro"]),
+         color='deeppink', linestyle=':', linewidth=4)
+
+plt.plot(fpr["macro"], tpr["macro"],
+         label='macro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["macro"]),
+         color='navy', linestyle=':', linewidth=4)
+
+colors = cycle(['aqua', 'darkorange', 'cornflowerblue', 'green', 'red'])
+# for i, color in zip(range(n_classes), colors):
+#     plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+#              label='ROC curve of class {0} (area = {1:0.2f})'
+#              ''.format(i, roc_auc[i]))
+    
+for i, color in zip(range(n_classes), colors):
+    plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+             label='ROC curve of class {0} (area = {1:0.2f})'
+             ''.format(classnames[i], roc_auc[i]))
+
+plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic curves')
+plt.legend(loc="lower right")
+plt.show()
 
 
 
