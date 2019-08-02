8d944af352993b43a617a6aef270cc4a2e0551ec
Sara Kassani <kassani.sara@gmail.com>
2019-08-02 10:09:07 -0600

    Update 0.utility_codes.py

--- 0.utility_codes.py
+++ 0.utility_codes.py
@@ -32,11 +32,13 @@
 
 
 from tensorflow.python.client import device_lib
+
 def get_available_gpus():
     local_device_protos = device_lib.list_local_devices()
     return [x.name for x in local_device_protos if x.device_type == 'GPU']
 
 GPUs = get_available_gpus()
+print(GPUs)
 
 
 ###########################################################################################################################
